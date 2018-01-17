3e1ffde74272b8c5849c8d19f9b44bd3057f89d9
duke <nolansingroy>
2018-01-17 16:52:50 -0600

    app.js conversion to MVO Working on converting the spegetti code to MVO .. img are not working at this commit but page is rendering

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 cat-clicker
 ===========
-# Cat-Clicker-refacator
+# Cat-Clicker-refactor Project
+
+The goal is to re-factor andy's spaghetti code using the MVO paradigm. This can be view by comparing the code using the commits for this project.
+
+#Credit Udacity Full stack web developer nano-degree Javascript Design Patterns
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+/**
 var cats = $(".cat");
 var buttons = $("button");
 
@@ -33,4 +33,140 @@ for (var i=1; i<=cats.length; i++){
 }
 
 hideAllCats();
-$("#cat1").show();
+$("#cat1").show();*/
+
+/*===== Model ======*/
+// Any code for the back end server || database
+// within model we set currentCat to null then declare our cats within an array with a click count variable set to 0
+ var model = {
+	 currentCat:null,
+	 cats:[
+		 {
+			 clickCount:0,
+			 name : 'large cat',
+			 imgSrc : 'img/cat_picture1.jpg',
+		 },
+		 {
+			 clickCount: 0,
+			 name : 'small cat',
+			 imgSrc : 'img/cat_picture2.jpg'
+		 },
+		 {
+			 clickCount: 0,
+			 name: 'medium cat',
+			 imgSrc: 'img/cat_picture3.jpg'
+		 }
+	 ]
+ };
+
+
+/* == octopus == */
+// functionality code that connects model to views
+var octopus = {
+ // must always have an init function
+	init: function() {
+		//setting current cat to the first cat in array
+		model.currentCat = model.cats[0];
+
+		//tell our views to initialize
+		catListView.init();
+		catView.init();
+	},
+	{
+		getCurrentCat: function() {
+			return model.currentCat;
+		}
+	},
+	{
+		getCats: function() {
+			return model.cats;
+		}
+	},
+	//set the currently selsected cat to the object passed in
+	setCurrentCat: function(cat) {
+		model.currentCat = cat;
+	},
+	//increments the counter for the currently selected cat
+	incrementCounter: function() {
+		model.currentCat.clickCount++;
+		catView.render()
+	}
+
+}
+
+
+/* ==== View ===== */
+// this is the front end code ..user interface
+var catView = {
+	init: function() {
+		//store pointers to our DOM elements for easy access later
+		// here we are using the id's declared in index.html
+		this.catElem = document.getElementById('cat');
+	  this.catNameElem = document.getElementById('cat_name');
+		this.catImageElem = document.getElementById('cat_img');
+		this.countElem = document.getElementById('cat_count');
+
+	  //event listener for the onclick, to increment the cat's count variable
+		this.catImageElem.addEventListener('click', function(){
+			octopus.incrementCounter();
+		});
+
+		//render this view(update the DOM elemens with the right values)
+	  this.render();
+
+	},
+
+	render:function () {
+ //update the DOM ELements with values from the current Cat
+		var currentCat = octopus.getCurrentCat();
+		this.countElem.textContent = currentCat.clickCount;
+		this.catNameElem.textContent = currentCat.name;
+		this.catImageElem.src = currentCat.imgSrc;
+	}
+};
+
+var catListView = {
+
+	init: function () {
+		//store the DOm Elemet for easy access later
+		this.catListElem = document.getElementById('cat_list');
+
+		//render this view (update the DOM elements with the right values)
+		this.render();
+
+	},
+	render: function() {
+		var cat, elem, i;
+		//get the cats we will be rendering fromt he octopus
+		var cats = octopus.getCats();
+
+		//empty the cat list
+		this.catListElem.innerHTML = '';
+
+		//loop over the cats
+		for (i = 0; i < cats.length; i++) {
+			//this is the cat we are currently looping over
+			cat = cats[i];
+
+			//make a new cat list item and set its textt
+			elem = document.createElement('li');
+			elem.textContent = cat.name;
+
+			//onclick, setCurrentCat and render the catView
+			// (this uses our closure-in-loop trick to connect the value
+			// of the cat variable to the click event function)
+			elem.addEventListener('click', (function(catCopy) {
+				return function() {
+					octopus.setCurrentCat(catCopy);
+					catView.render();
+				}
+			})(cat));
+
+			//add the element to the list
+			this.catListElem.appendChild(elem);
+		}
+	}
+};
+
+//make it go
+octopus.init();
